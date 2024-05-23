04c5b5a6270d0e050a5d46ab50722a275e02d0c2
Madhusudhanreddymadupu <madhusudhanreddy19.madupu@gmil.com>
2024-05-23 12:41:44 +0530

    first

---
+++ oops.java
@@ -0,0 +1,25 @@
+class Main
+{
+	public static void main(String args [])
+	{
+		
+		Main.method1();
+			
+	}
+	public static void method1()
+	{
+		System.out.println("hello");
+	}
+	public static void method2(int x)
+	{
+		System.out.println("hello");
+	}
+	public static void method3(int x,char b)
+	{
+		System.out.println("hello");
+	}
+	public static void method4(char c,double a)
+	{
+		System.out.println("hello");
+	}
+}
